7545180f391c76546860a191f3a1622930460004
gmaksim <gmaksim@users.noreply.github.com>
2017-07-14 15:56:57 +0300

    update 'view db' functionality

--- aViewMode.py
+++ aViewMode.py
@@ -1,66 +1,85 @@
 #! mode for view information
 from tkinter import *
+from tkinter import messagebox
 import sqlite3
 import os
+
+
 output = Tk()
 output.title("View mode")
 output.geometry("600x700+100+100")
+main_folder = os.getcwd()
 conn = sqlite3.connect('DATA//firstBase.sqlite')
 cursor = conn.cursor()
 
+# make left listbox (credit line)
+cursor.execute('SELECT Name FROM ANAME')
+aname_table = cursor.fetchall()
+aname_list_box = Listbox()
+aname_list_box.place(relx=.30, rely=.1, height=130, width=130)
+for item in aname_table:
+    aname_list_box.insert(END, item)
 
-def main():
-    cursor.execute('SELECT Name FROM ANAME')
-    aname_table = cursor.fetchall()
-    aname_list_box = Listbox()
-    aname_list_box.place(relx=.30, rely=.1, height=130, width=130)
-    for item in aname_table:
-        aname_list_box.insert(END, item)
+# make empty right listbox (credit line credit agree.)
+aagre_list_box = Listbox()
+aagre_list_box.place(relx=.60, rely=.1, height=130, width=130)
+
+
+# take position in left lb, and fix it
+def past_cred_agr_in_lb(nothing):
+    position_in_lb = aname_list_box.curselection()  # take position in left LB
+    cursor.execute('SELECT idSend FROM ANAME WHERE id=?', (position_in_lb))  # select in db info with this idSend
+    id_selected = cursor.fetchall()
+    if id_selected == []:  # (!) think up - about another method  # make correct offset for db
+        id_selected = 1
+    else:  # 1 and >
+        id_selected = str(id_selected)  # convert list to str
+        id_selected = int(id_selected[2:-3]) + 1  # cut str then convert to int and +1
+        # to have correct offset (position in another table)
+
+    # make right listbox with correct ids
+    cursor.execute('SELECT Agreement FROM AAGRE WHERE idSend = (?) ORDER BY id DESC',
+                   (id_selected,))  # '()' and ',' - it's important!
+    aagre_table = cursor.fetchall()  # print(aagre_table)  # (!) think up - can possible to take arguments for chdir?
     aagre_list_box = Listbox()
     aagre_list_box.place(relx=.60, rely=.1, height=130, width=130)
+    for item in aagre_table:
+        aagre_list_box.insert(0, item)
 
-    def paste_agr(nothing):
-        position_in_lb = aname_list_box.curselection()
-        cursor.execute('SELECT idSend FROM ANAME WHERE id=?', (position_in_lb))
-        id_selected = cursor.fetchall()
-        if id_selected == []:  # (!) maybe bad coding style, need to think how to fix it
-            id_selected = 1
-        else:  # 1 and >
-            id_selected = str(id_selected)  # convert list to str
-            id_selected = int(id_selected[2:-3]) + 1  # cut str then convert to int and +1
-            # to have correct offset (position in another table)
-        cursor.execute('SELECT Agreement FROM AAGRE WHERE idSend = (?) ORDER BY id DESC',
-                       (id_selected,))  # '()' and ',' - it's important!
-        aagre_table = cursor.fetchall()
-        aagre_list_box = Listbox()
-        aagre_list_box.place(relx=.60, rely=.1, height=130, width=130)
-        for item in aagre_table:
-            aagre_list_box.insert(0, item)
+    # make down info with docs-folder of credit agree.
+    def past_doc_fold_from_cred_agr(nothing):
+        # take a name of subfolder (in clients/%cred.agr.%)
+        cursor.execute('SELECT Agreement, idSend FROM AAGRE WHERE idSend = (?)',
+                       (id_selected,))  # '()' and ',' - it's important!  # take cred.agr where needed credit line
+        list_of_aagre_lb = cursor.fetchall()
+        id_selected_2 = aagre_list_box.curselection()  # take position in cred.agr
+        id_selected_2 = str(id_selected_2)  # convert to str
+        id_selected_2 = int(id_selected_2[1:-2])  # cut str and convert to int
+        agree_id_folder = list_of_aagre_lb[id_selected_2]  # in cred.agr. listbox take info with correct position
+        agree_folder = agree_id_folder[0]  # take first param. (that's subfolder)
 
-        def paste_folders(nothing):  # need to fix on idSend
-            cursor.execute('SELECT Agreement FROM AAGRE WHERE idSend = (?) ORDER BY id DESC',
-                           (id_selected,))  # '()' and ',' - it's important!
-            aagre_table = cursor.fetchall()
-            for item in aagre_table:
-                print(item)
+        # take a name of main folder (in clients)
+        q = position_in_lb[0] + 1  # position in credit line + 1 (make correct offset)
+        cursor.execute('SELECT Name, id FROM ANAME WHERE id=(?)', (q,))  # select info ANAME table with current position
+        all_info = cursor.fetchall()
+        curr_line = (all_info[0])  # take all line with name and id
+        credline_folder = curr_line[0]  # tale only name of credit line (that's main folder)
 
-            # folder_name = str(aagre_list_box.curselection())
-            # folder_name = int(folder_name[1:-2]) + 1
-            # cursor.execute('SELECT Agreement FROM AAGRE WHERE id=?', (folder_name,))
-            # curr_fold_name = cursor.fetchall()
-            # print(curr_fold_name)
-            # curr_fold_name = str(curr_fold_name)
-            # curr_fold_name = curr_fold_name[3:-4]
-            # print(curr_fold_name)
+        dirs_doc_view(agree_folder, credline_folder, main_folder)
+
+    aagre_list_box.bind('<<ListboxSelect>>', past_doc_fold_from_cred_agr)
 
 
-        aagre_list_box.bind('<<ListboxSelect>>', paste_folders)
+def dirs_doc_view(agree_folder, credline_folder, main_folder):
+    os.chdir(main_folder)
+    b = os.path.exists('CLIENTS')
+    if b is False:
+        messagebox.showinfo('WARNING', 'Check CLIENTS folder')
+    os.chdir('CLIENTS')
+    os.chdir(credline_folder)
+    os.chdir(agree_folder)
+    print(os.getcwd())
 
-
-
-
-    aname_list_box.bind('<<ListboxSelect>>', paste_agr)
-
-main()
+aname_list_box.bind('<<ListboxSelect>>', past_cred_agr_in_lb)
 output.mainloop()
 conn.close()
--- bAddModeA.py
+++ bAddModeA.py
@@ -4,11 +4,12 @@ from tkinter import messagebox
 from datetime import date
 import os
 import sqlite3
-import bAddModeB
 
-add_mode_A = Tk()
-add_mode_A.title("Add new information. Part 1")
-add_mode_A.geometry("600x700+100+100")
+
+def get_current_date():
+    today = date.today()
+    creditLine_entry_date.insert(END, today)
+    creditLine_entry_agr_date.insert(END, today)
 
 
 def make_db():
@@ -23,7 +24,7 @@ def make_db():
         conn.close()
 
 
-def save_and_step_2():
+def save_start_info():
     credit_line_name_imp = str(creditLine_name.get())
     credit_line_agreement_imp = str(creditLine_agreement.get())
     credit_line_date_imp = str(creditLine_date.get())
@@ -58,16 +59,119 @@ def save_and_step_2():
         conn.commit()
         conn.close()
         add_mode_A.destroy()
-        bAddModeB.main(credit_line_name_imp, credit_line_agreement_imp)
+        files_widget(credit_line_name_imp, credit_line_agreement_imp)
 
 
-def get_current_date():
-    today = date.today()
-    creditLine_entry_date.insert(END, today)
-    creditLine_entry_agr_date.insert(END, today)
+def files_widget(credit_line_name_imp, credit_line_agreement_imp):
+    add_mode_B = Tk()
+    add_mode_B.title("Add new information. Part 2")
+    add_mode_B.geometry("600x700+100+100")
+
+    make_clients_folders(credit_line_name_imp, credit_line_agreement_imp)
+
+    label_start = Label(text="Now you need to put files in folders")
+    label_start.place(relx=.01, rely=.01, height=60, width=250)
+
+    off_corr_label = Label(text='Official correspondence', fg="#eee", bg="#333")
+    off_corr_label.place(relx=.01, rely=.10, height=25, width=150)
+    jud_label = Label(text='Judgments', fg="#eee", bg="#333")
+    jud_label.place(relx=.01, rely=.15, height=25, width=150)
+    cred_agr_label = Label(text='Credit Agreement', fg="#eee", bg="#333")
+    cred_agr_label.place(relx=.01, rely=.20, height=25, width=150)
+
+    check_off_corr_label = Label(text='', fg="#eee", bg="#cccccc")
+    check_off_corr_label.place(relx=.28, rely=.10, height=25, width=40)
+    check_jud_label = Label(text='', fg="#eee", bg="#cccccc")
+    check_jud_label.place(relx=.28, rely=.15, height=25, width=40)
+    check_cred_agr_label = Label(text='', fg="#eee", bg="#cccccc")
+    check_cred_agr_label.place(relx=.28, rely=.20, height=25, width=40)
+
+    analyze_folders_btn = Button(text='Check files', height=1, width=20, command=analyze_folders)
+    analyze_folders_btn.place(relx=.08, rely=.25, height=30, width=130)
+
+    add_mode_B.mainloop()
 
 
-# elements (step 1)
+def make_clients_folders(credit_line_name_imp, credit_line_agreement_imp):
+    b = os.path.exists('CLIENTS')
+    if b is False:
+        os.mkdir('CLIENTS')
+        messagebox.showinfo('Information', 'CLIENTS folder created')
+    os.chdir('CLIENTS')
+    b = os.path.exists(credit_line_name_imp)
+    if b is False:
+        q = credit_line_name_imp + '//' + credit_line_agreement_imp
+        os.makedirs(q)
+        os.chdir(q)
+        z = 0
+        folders = ('Official correspondence', 'Judgments', 'Credit Agreement')
+        d = len(folders)
+        while z != d:
+            folder = folders[z]
+            os.mkdir(folder)
+            z += 1
+    else:
+        print('realization for additional agreement (later)')
+
+
+def analyze_folders():
+    # to know add user file in folder or no
+    file_exists = []
+    work_dir = os.getcwd()
+    z = 0
+    all_dirs_list = os.listdir()
+    d = len(all_dirs_list)
+    while z != d:
+        curr_dir = all_dirs_list[z]
+        os.chdir(curr_dir)
+        file_in_folder = os.listdir()
+        if file_in_folder == []:
+            file_exists.append('#7d0541')  # no
+        else:
+            file_exists.append('#00ff7d')  # yes
+        os.chdir(work_dir)
+        z += 1
+
+    # colour of labels (y/n file in folder)
+    def count():  # "static" count
+        try:
+            count.a += 1
+        except AttributeError:
+            count.a = 0
+        return count.a
+
+    def make_color():
+        curr_color = file_exists[count()]
+        return curr_color
+
+    check_off_corr_label = Label(text='', fg="#eee", bg=make_color())
+    check_off_corr_label.place(relx=.28, rely=.10, height=25, width=40)
+    check_jud_label = Label(text='', fg="#eee", bg=make_color())
+    check_jud_label.place(relx=.28, rely=.15, height=25, width=40)
+    check_cred_agr_label = Label(text='', fg="#eee", bg=make_color())
+    check_cred_agr_label.place(relx=.28, rely=.20, height=25, width=40)
+
+    # check all files add or no
+    r = len(file_exists)
+    j = 0
+    q = '#00ff7d'
+    for i in file_exists:
+        if i == q:
+            j += 1
+    if j == r:
+        add_attribute()
+    else:
+        messagebox.showinfo('Information', 'Add all files, please')
+
+
+def add_attribute():
+    print('all pass')
+
+
+add_mode_A = Tk()
+add_mode_A.title("Add new information. Part 1")
+add_mode_A.geometry("600x700+100+100")
+
 labelStart = Label(text="Add name of organization and # of agreement")
 labelStart.place(relx=.01, rely=.01, height=60, width=250)
 
@@ -96,9 +200,9 @@ labelAgreement.place(relx=.01, rely=.20, height=25, width=60)
 labelAgrDate = Label(text="Agr. date", fg="#eee", bg="#333")
 labelAgrDate.place(relx=.01, rely=.25, height=25, width=60)
 
-get_current_date()
-make_db()
-creditLine_btn = Button(text='Save and next step', height=1, width=20, command=save_and_step_2)
+creditLine_btn = Button(text='Save and next step', height=1, width=20, command=save_start_info)
 creditLine_btn.place(relx=.12, rely=.30, height=30, width=130)
 
+get_current_date()
+make_db()
 add_mode_A.mainloop()
